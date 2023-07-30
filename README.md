d634fbcdf6fc70e652acad7b0ebe2605633cb57f
Lilian POULIQUEN <lil.pouliqu@gmail.com>
2023-07-30 20:53:44 +0200

    __init__

---
+++ README.md
@@ -0,0 +1,56 @@
+# Custom list character
+
+## Introduction
+
+Custom list character is a simple plugin allowing you to choose which character to use when making bullet lists in Obsidian.
+
+It adds the following features:
+
+* new settings to choose the wanted charater between `-` (default), `*` and `+`
+* new hotkey to bind to use the selected character to make or format your bullet lists
+
+## Demo
+
+1. Choose your character!
+
+![Custom list character settings](./docs/images/custom_list_character_settings.png "Choosing my custom list character")
+
+2. Set your hotkeys:
+
+![Custom list character hotkeys](./docs/images/custom_list_character_hotkeys.png "Setting up my hotkeys to make bullet lists with the custom character")
+
+3. Use the new hotkey to make and format your bullet lists like so:
+
+![Custom list character demo](./docs/images/custom_list_character_demo.gif "Formatting and making bullet lists with the character")
+
+## Install the plugin
+
+### Installatin through Obsidian
+
+To install the plugin from Obsidian, follow these steps:
+
+1. Open Obsidian
+2. Go to `Settings > Community plugins`
+3. Click on `Brownse` in `Community plugins` section
+4. Search `Custom list character`
+5. Click on the plugin
+6. Click on `Install`
+7. Come back to `Settings > Community plugins`
+8. Refresh you plugins and activate `Custom list character`
+
+### Manual installation
+
+To install the plugin, follow these steps:
+
+1. Download the `main.js` and the `manifest.json` from the [latest release à the project](https://github.com/lilian-pouliquen/obsidian-custom-list-character/releases/latest/)
+   * Alternatively you can download the `custom-list-character.zip` that contains the two above files
+2. Place the files in `<your_vault_path>/.obsidian/plugins/custom-list-character/`
+3. Open Obsidian
+4. Go to `Settings > Community plugins`
+5. Refresh you plugins and activate `Custom list character`
+
+## Help on the project
+
+Any help is welcome!
+
+If you want to help me improve the plugin, please fork the project and make a pull request. Do not hesitate to ask features.
---
+++ main.ts
@@ -0,0 +1,138 @@
+import { App, Editor, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface CustomListCharacterSettings {
+    listCharacter: string;
+}
+
+const DEFAULT_SETTINGS: CustomListCharacterSettings = {
+    listCharacter: '-'
+}
+
+export default class CustomListCharacterPlugin extends Plugin {
+    settings: CustomListCharacterSettings;
+
+    async onload() {
+        await this.loadSettings();
+
+        // Add an editor command that adds the custom list character befor each line of the selection
+        this.addCommand({
+            id: 'toggle-or-format-bullet-list',
+            name: 'Toggle or format bullet list',
+            editorCallback: (editor: Editor) => {
+                this.setBulletList(editor)
+            }
+        });
+
+        // Add setting tab
+        this.addSettingTab(new CustomListCharacterSettingTab(this.app, this));
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+
+    // Format the selection to make a bullet list with the custom character
+    private setBulletList(editor: Editor) {
+        let processedSelection = "";
+
+        // Select whole lines of the selection
+        const selectionRange = this.getSelection(editor);
+        editor.setSelection(selectionRange.begin, selectionRange.end);
+
+        // Cleaning the selection
+        let selection = editor.getSelection().split('\n');
+        selection = this.trimLines(selection);
+
+        // Check if selection is already a well formated bullet list
+        // If true, remove the bullet list
+        // Else, format the selection to make a bullet list with the custom character
+        if (this.isBulletList(selection)) {
+            for (let line of selection) {
+                if (line != "") {
+                    line = line.substring(2);
+                }
+                processedSelection += line + '\n';
+            }
+        }
+        else {
+            for (let line of selection) {
+                if (line.startsWith("- ") || line.startsWith("* ") || line.startsWith("+ ")) {
+                    line = line.substring(2);
+                }
+                if (line != "") {
+                    processedSelection += this.settings.listCharacter + ' ' + line + '\n';
+                } else {
+                    processedSelection += line + '\n';
+                }
+            }
+        }
+
+        // Remove extra new line
+        processedSelection = processedSelection.substring(0, processedSelection.length - 1)
+
+        // Replace selection with the bullet list and keep selection active
+        editor.replaceSelection(processedSelection);
+        selectionRange.end.ch = editor.getLine(selectionRange.end.line).length;
+        editor.setSelection(selectionRange.begin, selectionRange.end);
+    }
+
+    // Get the range of the selection, adding parts of the lines that are not selected
+    private getSelection(editor: Editor) {
+        const selectionBeginLine = editor.getCursor("from").line;
+        const selectionEndLine = editor.getCursor("to").line;
+        const endLineLength = editor.getLine(selectionEndLine).length;
+        return { begin: { ch: 0, line: selectionBeginLine }, end: { ch: endLineLength, line: selectionEndLine } };
+    }
+
+    // Trim all lines
+    private trimLines(selection: Array<string>) {
+        const trimedSelection = [];
+        for (const line of selection) {
+            trimedSelection.push(line.trim());
+        }
+        return trimedSelection;
+    }
+
+    // Check if all selected lines start with the custom character or is an empty line
+    private isBulletList(selection: Array<string>) {
+        let customListCharacterCount = 0;
+        for (const line of selection) {
+            if (line.startsWith(this.settings.listCharacter + ' ') || line == "") {
+                customListCharacterCount++
+            }
+        }
+        return selection.length == customListCharacterCount;
+    }
+}
+
+class CustomListCharacterSettingTab extends PluginSettingTab {
+    plugin: CustomListCharacterPlugin;
+
+    constructor(app: App, plugin: CustomListCharacterPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const { containerEl } = this;
+
+        containerEl.empty();
+
+        new Setting(containerEl)
+            .setName('List character')
+            .setDesc('Use this character to make your bullet lists')
+            .addDropdown(dropDown => dropDown
+                .addOption('-', '-')
+                .addOption('*', '*')
+                .addOption('+', '+')
+                .setValue(this.plugin.settings.listCharacter)
+                .onChange(async (value) => {
+                    this.plugin.settings.listCharacter = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
+}
